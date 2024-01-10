b844095c8ed72df6796582bcfdc1c060c7bdc46e
wds33817 <wds33817>
2024-01-10 23:14:03 +1100

    add more fetching api

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -20,6 +20,7 @@ export default function Form() {
 
       <Nameform />
       <Statistics />
+      <FetchUsers />
     </Fragment>
   );
 }
@@ -57,5 +58,29 @@ function Statistics() {
         <p>Data pending ...</p>
       )}
     </>
+  );
+}
+
+function FetchUsers() {
+  const [user, setUser] = useState([]);
+
+  const fetchData = () => {
+    fetch('https://randomuser.me/api/?results=1')
+      .then((response) => response.json())
+      .then((data) => setUser(data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return Object.keys(user).length > 0 ? (
+    <div>
+      <h1>Data returned</h1>
+      <h2>First Name: {user.results[0].name.first}</h2>
+      <h2>Last Name: {user.results[0].name.last}</h2>
+    </div>
+  ) : (
+    <h1>Data pending...</h1>
   );
 }
